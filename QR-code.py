ed67151969e9f6a0e7fd718c475795871cd270b7
PetrashchukIM <petrashchukim@gmail.com>
2024-09-23 16:30:42 +0300

    add QR function

--- QR-code.py
+++ QR-code.py
@@ -4,33 +4,34 @@ from PIL import Image
 def extract_qr_code(image_path, output_path):
     # Завантаження зображення за допомогою OpenCV
     image = cv2.imread(image_path)
-    
+
+    if image is None:
+        print("Не вдалося завантажити зображення.")
+        return
+
     # Створюємо QR-код детектор
     qr_detector = cv2.QRCodeDetector()
 
     # Визначаємо QR-код на зображенні
     data, points, _ = qr_detector.detectAndDecode(image)
 
-    if points is not None:
+    if points is not None and len(points) > 0:
         # Якщо QR-код знайдено, витягуємо координати його кутів
         points = points[0]
 
-        # Отримуємо координати (якщо QR-код перекошений, їх буде 4)
-        x1, y1 = points[0]
-      
-        # Обрізаємо зображення, вирізаючи QR-код за допомогою координат
-        x_min = min(int(x1), int(x2), int(x3), int(x4))
-        y_min = min(int(y1), int(y2), int(y3), int(y4))
-        x_max = max(int(x1), int(x2), int(x3), int(x4))
-        y_max = max(int(y1), int(y2), int(y3), int(y4))
+        # Отримуємо координати
+        x_min = int(min(points[:, 0]))
+        y_min = int(min(points[:, 1]))
+        x_max = int(max(points[:, 0]))
+        y_max = int(max(points[:, 1]))
 
         # Обрізаємо зображення і зберігаємо результат
         cropped_image = image[y_min:y_max, x_min:x_max]
         cropped_image_pil = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
         cropped_image_pil.save(output_path)
-        print(f"QR-код збережено як {output_path}")
+        print(f"QR-save here {output_path}")
     else:
-        print("QR-код не знайдено на зображенні.")
+        print("QR-not found.")
 
 # Виклик функції
 extract_qr_code("1453.png", "output_qr.png")
