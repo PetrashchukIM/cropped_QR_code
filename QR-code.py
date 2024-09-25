72f528baaf022d9b4a34b75415e7ffaf01c7c025
PetrashchukIM <petrashchukim@gmail.com>
2024-09-25 14:44:27 +0300

    chanche function

--- QR-code.py
+++ QR-code.py
@@ -4,8 +4,9 @@ import os
 import shutil
 
 OUTPUT_PATH='output_QR_code'
+TMP_PATH='temp'
 
-def extract_qr_codes(image_path, output_path1, output_path2):
+def extract_qr_codes(image_path):
     print(f'Start extracting {image_path}')
     # Load the image using OpenCV
     image = cv2.imread(image_path)
@@ -24,50 +25,53 @@ def extract_qr_codes(image_path, output_path1, output_path2):
     left_half = image[:, :part_width]
     right_half = image[:, (count_part-1) * part_width:]
 
-def extract_and_save_img(half, name_file)
+    extract_and_save_img (left_half,'1.png')
+    extract_and_save_img (right_half,'2.png')
+
+
+def extract_and_save_img(half, name_file):
     half_rgb = cv2.cvtColor(half, cv2.COLOR_BGR2RGB)
     half_pil = Image.fromarray(half_rgb)
-    half_pil.save(os.path.join(OUTPUT_PATH, name_file))
-    print("Right half saved as 'right_half_output.png'")
-    print(f'{width},{height}')
+    half_pil.save(os.path.join(TMP_PATH, name_file))
+    print(f"File {name_file} saved")
 
 
     
     # Detect and decode the QR codes
-    data1, points1, _ = qr_detector.detectAndDecode(left_half)
-    if points1 is not None and len(points1) > 0:
-        points1 = points1[0]
-        x_min1 = int(min(points1[:, 0]))
-        y_min1 = int(min(points1[:, 1]))
-        x_max1 = int(max(points1[:, 0]))
-        y_max1 = int(max(points1[:, 1]))
+    # data1, points1, _ = qr_detector.detectAndDecode(left_half)
+    # if points1 is not None and len(points1) > 0:
+    #     points1 = points1[0]
+    #     x_min1 = int(min(points1[:, 0]))
+    #     y_min1 = int(min(points1[:, 1]))
+    #     x_max1 = int(max(points1[:, 0]))
+    #     y_max1 = int(max(points1[:, 1]))
 
-        # Crop the first QR code
-        cropped_image1 = image[y_min1:y_max1, x_min1:x_max1]
-        cropped_image1_pil = Image.fromarray(cv2.cvtColor(cropped_image1, cv2.COLOR_BGR2RGB))
-        cropped_image1_pil.save(output_path1)
-        print(f"First QR code saved as {output_path1}")
-        print("Extracted data:", data1)
-    else:
-        print("First QR code not found.")
+    #     # Crop the first QR code
+    #     cropped_image1 = image[y_min1:y_max1, x_min1:x_max1]
+    #     cropped_image1_pil = Image.fromarray(cv2.cvtColor(cropped_image1, cv2.COLOR_BGR2RGB))
+    #     cropped_image1_pil.save(output_path1)
+    #     print(f"First QR code saved as {output_path1}")
+    #     print("Extracted data:", data1)
+    # else:
+    #     print("First QR code not found.")
 
-    # Detect and decode the second QR code
-    data2, points2, _ = qr_detector.detectAndDecode(right_half)
-    if points2 is not None and len(points2) > 0:
-        points2 = points2[0]
-        x_min2 = int(min(points2[:, 0]))
-        y_min2 = int(min(points2[:, 1]))
-        x_max2 = int(max(points2[:, 0]))
-        y_max2 = int(max(points2[:, 1]))
+    # # Detect and decode the second QR code
+    # data2, points2, _ = qr_detector.detectAndDecode(right_half)
+    # if points2 is not None and len(points2) > 0:
+    #     points2 = points2[0]
+    #     x_min2 = int(min(points2[:, 0]))
+    #     y_min2 = int(min(points2[:, 1]))
+    #     x_max2 = int(max(points2[:, 0]))
+    #     y_max2 = int(max(points2[:, 1]))
 
-        # Crop the second QR code
-        cropped_image2 = image[y_min2:y_max2, x_min2:x_max2]
-        cropped_image2_pil = Image.fromarray(cv2.cvtColor(cropped_image2, cv2.COLOR_BGR2RGB))
-        cropped_image2_pil.save(output_path2)
-        print(f"Second QR code saved as {output_path2}")
-        print("Extracted data:", data2)
-    else:
-        print("Second QR code not found.")
+    #     # Crop the second QR code
+    #     cropped_image2 = image[y_min2:y_max2, x_min2:x_max2]
+    #     cropped_image2_pil = Image.fromarray(cv2.cvtColor(cropped_image2, cv2.COLOR_BGR2RGB))
+    #     cropped_image2_pil.save(output_path2)
+    #     print(f"Second QR code saved as {output_path2}")
+    #     print("Extracted data:", data2)
+    # else:
+    #     print("Second QR code not found.")
 
 
 def check_and_remove_directory(directory_path):
@@ -86,6 +90,8 @@ def create_directory(directory_path):
 
 check_and_remove_directory(OUTPUT_PATH)
 create_directory(OUTPUT_PATH)
+check_and_remove_directory(TMP_PATH)
+create_directory(TMP_PATH)
 
 output_path1 = os.path.join(OUTPUT_PATH, "output_qr1.png")
 output_path2 = os.path.join(OUTPUT_PATH, "output_qr2.png")
