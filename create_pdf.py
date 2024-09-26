cd1cd8877c9dcdb715ffeb48c9c070a91894e81e
PetrashchukIM <petrashchukim@gmail.com>
2024-09-26 13:07:49 +0300

    change pdf function

--- create_pdf.py
+++ create_pdf.py
@@ -2,42 +2,46 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
 
-# Функція для створення PDF
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+X_OFFSETS = 20
+Y_OFFSETS = 20
+IMGS_BTW_WIDTH = 10
+IMGS_BTW_HEIGHT = 10
+
 def create_pdf(image_paths, output_pdf_path):
     c = canvas.Canvas(output_pdf_path, pagesize=letter)
-    width, height = letter  # Розміри сторінки
-
-    # Визначте розміри картинок
-    img_width = 200
-    img_height = 200
-    x_offset = 50
-    y_offset = height - img_height - 50  # Відстань зверху
+    width, height = letter 
+    
+    y_offset = height - IMG_HEIGHT - IMGS_BTW_HEIGHT # Відстань зверху
 
     for i, image_path in enumerate(image_paths):
-        if i > 0 and i % 5 == 0:  # Якщо 5 картинок, переходьте на нову сторінку
+        if i > 0 and i % 5 == 0:  
             c.showPage()
-            y_offset = height - img_height - 50  # Скидання відстані зверху
+            y_offset = height - IMG_HEIGHT - IMGS_BTW_HEIGHT  
 
-        c.drawImage(image_path, x_offset, y_offset, width=img_width, height=img_height)
-        x_offset += img_width + 10  # Збільшуємо відстань між картинками
+        c.drawImage(image_path, X_OFFSETS, Y_OFFSETS, width=IMG_WIDTH, height=IMG_HEIGHT)
+        x_offset += IMG_WIDTH + IMGS_BTW_WIDTH  
 
-        if x_offset + img_width > width:  # Якщо не вміщаються в ряд, переходьте на новий ряд
-            x_offset = 50
-            y_offset -= img_height + 10  # Збільшуємо відстань між рядами
+        if x_offset + IMG_WIDTH > width:  
+            x_offset = X_OFFSETS
+            y_offset -= IMG_HEIGHT + Y_OFFSETS  
 
     c.save()
 
-# Шляхи до картинок
 image_paths = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg",
-    "image4.jpg",
-    "image5.jpg"
+    "output_QR_code\one_qr_1453.png",
+    "output_QR_code\one_qr_1454.png",
+    "output_QR_code\one_qr_1455.png",
+    "output_QR_code\one_qr_1456.png",
+    "output_QR_code\one_qr_1457.png",
+    "output_QR_code\second_qr_1453.png",
+    "output_QR_code\second_qr_1454.png",
+    "output_QR_code\second_qr_1455.png",
+    "output_QR_code\second_qr_1456.png",
+    "output_QR_code\second_qr_1457.png"
 ]
 
-# Вихідний PDF файл
-output_pdf_path = "output.pdf"
+output_pdf_path = "output_QR\output.pdf"
 
-# Викликаємо функцію для створення PDF
 create_pdf(image_paths, output_pdf_path)
